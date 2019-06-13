d632ba8f4170089dda8a01938e7f1470ac5e5fc4
Charlotte Kaandorp <39331760+charlottekaandorp@users.noreply.github.com>
2019-06-13 12:01:02 +0200

    invalid date input fix

--- timely-beliefs-master/timely_beliefs/visualization/ridgeline.py
+++ timely-beliefs-master/timely_beliefs/visualization/ridgeline.py
@@ -15,7 +15,6 @@ import pandas as pd
 def ridgeline_plot(date, csv_05, csv_095, output=False, start=0, end=168):
     """ 
     Creates rigdeline plot 
-    
     @param date : datetime string
     @param csv_05 : input csv file with 50 percent probability
     @param csv_095 : input csv file with 95 percent probability
@@ -29,8 +28,9 @@ def ridgeline_plot(date, csv_05, csv_095, output=False, start=0, end=168):
         start = 0
     start_index = start + 2
     end_index = end + 2
-    data_05 = create_data(csv_05)
-    data_095 = create_data(csv_095)
+    # load csv and remove headers
+    data_05 = create_data(csv_05)[1:]
+    data_095 = create_data(csv_095)[1:]
     index_05 = get_row(date, data_05)
     index_095 = get_row(date, data_095)
     pred_temp_05 = data_05[index_05][start_index:end_index]
@@ -43,10 +43,10 @@ def ridgeline_plot(date, csv_05, csv_095, output=False, start=0, end=168):
     show_plot(mean, sigma, nr_lines)
 
 
+
 def show_plot(mean, sigma, nr_lines):
     """
     Creates and shows ridgeline plot
-
     @param mean: list of mean values
     @param sigma: list of sigma values
     @param nr_lines: number of pdfs to be drawn
@@ -90,26 +90,32 @@ def ridge(category, data, scale=100):
     return list(zip([category] * len(data), scale * data))
 
 
-def get_row(current_time, data):
+def get_row(current_time,data):
     """
     Returns index of current_time in data
 
     @param current_time : datetime string to be searched
     @param data : read csv file
     """
-    datetime_object = datetime.datetime.strptime(current_time, '%Y-%m-%d %H:%M:%S')
+    #convert string to datetime object for comparing
+    datetime_object = datetime.datetime.strptime(current_time,'%Y-%m-%d %H:%M:%S')
+    #set left and right halfs
     L = 0
     R = len(data) - 1
     lastindex = 0
-    while(True):
-        index = math.floor((L + R) / 2)
+    while(L <= R):
+        #set middle point/ search index
+        index = math.floor((L+R)/2)
+        #round to closest value if exact value not found
         if index == lastindex:
             break
         lastindex = index
-        if datetime.datetime.strptime(data[index][0][:-6], '%Y-%m-%d %H:%M:%S') == datetime_object:
+        #if time found return
+        if datetime.datetime.strptime(data[index][0][:-6],'%Y-%m-%d %H:%M:%S') == datetime_object:
             break
-        elif datetime.datetime.strptime(data[index][0][:-6], '%Y-%m-%d %H:%M:%S') > datetime_object:
-            R = index - 1
+        elif datetime.datetime.strptime(data[index][0][:-6],'%Y-%m-%d %H:%M:%S') > datetime_object:
+            if index > 0:
+                R = index - 1
         else:
             L = index + 1
     return index
@@ -118,7 +124,6 @@ def get_row(current_time, data):
 def create_data(csv_in):
     """
     Returns data as list type
-
     @param csv_in : input csv file  
     """
     with open(csv_in) as csv_file:
@@ -127,4 +132,4 @@ def create_data(csv_in):
     return data
 
 
-# ridgeline_plot('2015-06-16 10:30:00', 'temperature-random_forest-0.5.csv', 'temperature-random_forest-0.95.csv')
+ridgeline_plot('2015-08-16 10:30:00', 'temperature-random_forest-0.5.csv', 'temperature-random_forest-0.95.csv')
